398df4f9d74641bb2859de8e8dc770327b681e4d
Maxime Bailly <maxime.bailly@etu.minesparis.psl.eu>
2025-07-01 11:14:11 +0200

    kmeans++

--- clusters.py
+++ clusters.py
@@ -6,6 +6,7 @@ from sklearn.cluster import KMeans
 img = cv2.imread('data_hsv/impressionist_paintings/0.jpg', cv2.IMREAD_COLOR)
 
 
+
 def hsv_distance(p1, p2):  # p1 et p2 sont des triplets de la forme [h,s,v]
     r1 = (p1[1] / 255) * (p1[2] / 255)
     theta1 = (p1[0] / 180) * 2 * np.pi
@@ -18,13 +19,28 @@ def hsv_distance(p1, p2):  # p1 et p2 sont des triplets de la forme [h,s,v]
     x2 = r2 * np.cos(theta2)
     y2 = r2 * np.sin(theta2)
 
-    return (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)**0.5
 
 def moyenne(cluster):
     """Calcul le centre (couleur moyenne) d'un cluster."""
     if not cluster:
         return np.array([0, 0, 0])
     return np.mean(cluster, axis=0)
+
+def initialize(data, n):
+    centroids = []
+    centroids.append(data[np.random.randint(data.shape[0])])
+
+    for _ in range(n - 1):
+        distances = []
+        for point in data:
+            min_dist = min([hsv_distance(point, c) for c in centroids])
+            distances.append(min_dist)
+        
+        next_centroid = data[np.argmax(distances)]
+        centroids.append(next_centroid)
+    
+    return np.array(centroids)
 
 class KMeans:
     def __init__(self, n_clusters, max_iter=10):
@@ -33,8 +49,10 @@ class KMeans:
 
     def fit(self, X_train):
         # Initialize centroids
-        min_, max_ = np.min(X_train, axis=0), np.max(X_train, axis=0)
-        self.centroids = [np.random.uniform(min_, max_) for _ in range(self.n_clusters)]
+        # Run initialization
+        self.centroids = initialize(X_train, n=self.n_clusters)
+        #min_, max_ = np.min(X_train, axis=0), np.max(X_train, axis=0)
+        #self.centroids = [np.random.uniform(min_, max_) for _ in range(self.n_clusters)]
 
         # Iterate until convergence or max iterations
         iteration = 0
@@ -65,31 +83,19 @@ class KMeans:
 #idealement renvoie d[couleur] = population
 
 #print(img.reshape(-1, 3).shape)  # Reshape the image to a 2D array of pixels
-kmeans = KMeans(n_clusters=4).fit(img.reshape(-1, 3))
+kmeans = KMeans(n_clusters=7).fit(img.reshape(-1, 3))
 
+#convert the clusters from HSV to RGB
+for i, (centroid, population) in kmeans.items():
+    centroid = np.clip(centroid, 0, 255).astype(int)
+    kmeans[i] = (cv2.cvtColor(np.array([[centroid]], dtype=np.uint8), cv2.COLOR_HSV2RGB)[0][0], population)
 
-
-
-        
-            
-
-
-
-    
-
-
-    
-
-  
-
-
-
-
-    
-
-
-        
-
-
-
-
+#Plot the clusters
+plt.figure(figsize=(10, 5))
+for i, (centroid, population) in kmeans.items():
+    plt.bar(i, population, color=centroid / 255, label=f'Cluster {i} (Population: {population})')
+plt.xlabel('Cluster')
+plt.ylabel('Population')
+plt.title('Population of Clusters')
+plt.legend()
+plt.show()
