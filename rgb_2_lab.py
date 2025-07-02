ca269a7d153d02eb82542df0c808457798e968a2
Arnaud Deltour <arnaud.deltour@etu.minesparis.psl.eu>
2025-07-02 15:10:15 +0200

    lab dataset

---
+++ rgb_2_lab.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+dataset_dir = 'abstract_compressed'
+dataset_hsv_dir = 'abstract_lab'
+
+# Créer dossier de sortie s'il n'existe pas
+os.makedirs(dataset_hsv_dir, exist_ok=True)
+
+# Liste des images dans le dossier
+image_list = [os.path.join(dataset_dir, file) for file in os.listdir(dataset_dir) if file.lower().endswith('.png')]
+
+i = 0
+for image_path in image_list:
+    img = cv2.imread(image_path)
+
+    # Convertir en HSV
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+    # Extraire nom de fichier proprement
+    name = os.path.basename(image_path)
+
+    # Sauvegarder l'image HSV
+    cv2.imwrite(os.path.join(dataset_hsv_dir, name), hsv_img)
+    i += 1
+
+print(f"Conversion terminée pour {i} images")
